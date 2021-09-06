a003493b9122a2c199ce712647aac59e990357ca
Phil Huffman <56phil@users.noreply.github.com>
2021-09-06 07:27:43 -0500

    now prettier

--- OriginalDLX/main.cpp
+++ OriginalDLX/main.cpp
@@ -13,15 +13,15 @@ using namespace std;
 
 typedef array<array<int, SIZE>, SIZE> puzzleType;
 
-const int SIZE_SQUARED = SIZE*SIZE;
 const int SIZE_SQRT = 3;
-const int ROW_NB = SIZE*SIZE*SIZE;
-const int COL_NB = 4 * SIZE*SIZE;
+const int SIZE_SQUARED = SIZE * SIZE;
+const int ROW_NB = SIZE_SQUARED * SIZE;
+const int COL_NB = SIZE_SQUARED << 2;
 
-struct Node Head;
-struct Node* HeadNode = &Head;
-struct Node* solution[MAX_K];
-struct Node* orig_values[MAX_K];
+Node Head;
+Node* HeadNode = &Head;
+Node* solution[MAX_K];
+Node* orig_values[MAX_K];
 
 bool matrix[ROW_NB][COL_NB] = { { 0 } };
 bool isSolved = false;
@@ -32,9 +32,10 @@ void stringToPuzzleType(string&, puzzleType&);
 
 clock_t timer, timer2;
 
-//===============================================================================================================//
-//---------------------------------------------DLX Functions-----------------------------------------------------//
-//===============================================================================================================//
+
+//===========================================================================//
+// DLX Functions ------------------------------------------------------------//
+//===========================================================================//
 
 void coverColumn(Node* col) {
     col->left->right = col->right;
@@ -67,13 +68,14 @@ void search(int k) {
         puzzleType grid;
         mapSolutionToGrid(grid);
         printGrid(grid);
-        std::cout << "Time Elapsed: " << (float)timer2 / CLOCKS_PER_SEC << " seconds.\n\n";
+        std::cout << "Time Elapsed: " << static_cast<double>(timer2) / CLOCKS_PER_SEC
+        << " seconds.\n\n";
         timer = clock();
         isSolved = true;
         return;
     }
 
-    //Choose Column Object Deterministically: Choose the column with the smallest Size
+    // Choose Column Object Deterministically: Choose the column with the smallest Size
     Node* Col = HeadNode->right;
     for (Node* temp = Col->right; temp != HeadNode; temp = temp->right)
         if (temp->size < Col->size)
@@ -100,14 +102,15 @@ void search(int k) {
     uncoverColumn(Col);
 }
 
-//===============================================================================================================//
-//----------------------Functions to turn a Sudoku grid into an Exact Cover problem -----------------------------//
-//===============================================================================================================//
 
-//--------------------------BUILD THE INITIAL MATRIX CONTAINING ALL POSSIBILITIES--------------------------------//
+//===========================================================================//
+// Functions to turn a Sudoku grid into an Exact Cover problem --------------//
+//===========================================================================//
+
+// BUILD THE INITIAL MATRIX CONTAINING ALL POSSIBILITIES --------------------//
 void BuildSparseMatrix(bool matrix[ROW_NB][COL_NB]) {
 
-    //Constraint 1: There can only be one value in any given cell
+    // 1: There can only be one value in any given cell
     int j = 0, counter = 0;
     for (int i = 0; i < ROW_NB; i++) { //iterate over all rows
         matrix[i][j] = 1;
@@ -118,7 +121,7 @@ void BuildSparseMatrix(bool matrix[ROW_NB][COL_NB]) {
         }
     }
 
-    //Constraint 2: There can only be one instance of a number in any given row
+    // 2: There can only be one instance of a number in any given row
     int x = 0;
     counter = 1;
     for (j = SIZE_SQUARED; j < 2 * SIZE_SQUARED; j++) {
@@ -133,7 +136,7 @@ void BuildSparseMatrix(bool matrix[ROW_NB][COL_NB]) {
             x++;
     }
 
-    //Constraint 3: There can only be one instance of a number in any given column
+    // 3: There can only be one instance of a number in any given column
     j = 2 * SIZE_SQUARED;
     for (int i = 0; i < ROW_NB; i++)
     {
@@ -143,7 +146,7 @@ void BuildSparseMatrix(bool matrix[ROW_NB][COL_NB]) {
             j = 2 * SIZE_SQUARED;
     }
 
-    //Constraint 4: There can only be one instance of a number in any given region
+    // 4: There can only be one instance of a number in any given 3x3 region
     x = 0;
     for (j = 3 * SIZE_SQUARED; j < COL_NB; j++) {
 
@@ -163,7 +166,7 @@ void BuildSparseMatrix(bool matrix[ROW_NB][COL_NB]) {
     }
 }
 
-//-------------------BUILD A TOROIDAL DOUBLY LINKED LIST OUT OF THE SPARSE MATRIX-------------------------//
+// BUILD A TOROIDAL DOUBLY LINKED LIST OUT OF THE SPARSE MATRIX -------------//
 void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
 
     Node* header = new Node;
@@ -175,7 +178,7 @@ void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
     header->head = header;
     Node* temp = header;
 
-    //Create all Column Nodes
+    // Create all Column Nodes
     for (int i = 0; i < COL_NB; i++) {
         Node* newNode = new Node;
         newNode->size = 0;
@@ -189,7 +192,8 @@ void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
     }
 
     int ID[3] = { 0,1,1 };
-    //Add a Node for each 1 present in the sparse matrix and update Column Nodes accordingly
+    // Add a Node for each 1 present in the sparse matrix and
+    // update Column Nodes accordingly
     for (int i = 0; i < ROW_NB; i++) {
         Node* top = header->right;
         Node* prev = NULL;
@@ -202,8 +206,7 @@ void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
         else if (i!= 0 && i%SIZE == 0) {
             ID[0] -= SIZE - 1;
             ID[2]++;
-        }
-        else {
+        } else {
             ID[0]++;
         }
 
@@ -237,7 +240,7 @@ void BuildLinkedList(bool matrix[ROW_NB][COL_NB]) {
     HeadNode = header;
 }
 
-// COVERS VALUES THAT ARE ALREADY PRESENT IN THE GRID -----------------------------------------------------//
+// COVERS VALUES THAT ARE ALREADY PRESENT IN THE GRID -----------------------//
 void TransformListToCurrentGrid(puzzleType& puzzle) {
     int index = 0;
     for(int i = 0 ; i<SIZE; i++ )
@@ -247,7 +250,9 @@ void TransformListToCurrentGrid(puzzleType& puzzle) {
                 Node* temp = NULL;
                 for (Col = HeadNode->right; Col != HeadNode; Col = Col->right) {
                     for (temp = Col->down; temp != Col; temp = temp->down)
-                        if (temp->rowID[0] == puzzle[i][j] && (temp->rowID[1] - 1) == i && (temp->rowID[2] - 1) == j)
+                        if (temp->rowID[0] == puzzle[i][j] &&
+                            temp->rowID[1] - 1 == i &&
+                            temp->rowID[2] - 1 == j)
                             goto ExitLoops;
                 }
             ExitLoops:
@@ -257,9 +262,7 @@ void TransformListToCurrentGrid(puzzleType& puzzle) {
                 for (Node* node = temp->right; node != temp; node = node->right) {
                     coverColumn(node->head);
                 }
-
             }
-
 }
 
 void SolveSudoku(puzzleType& sudoku) {
@@ -273,21 +276,22 @@ void SolveSudoku(puzzleType& sudoku) {
     isSolved = false;
 }
 
-//=====================================================================================================//
-//------------------------------------- Print Functions -----------------------------------------------//
-//=====================================================================================================//
+//===========================================================================//
+// Print Functions ----------------------------------------------------------//
+//===========================================================================//
 
 void mapSolutionToGrid(puzzleType& sudoku) {
 
     for (int i = 0; solution[i] != NULL; i++) {
         sudoku[solution[i]->rowID[1]-1][solution[i]->rowID[2]-1] = solution[i]->rowID[0];
     }
+    
     for (int i = 0; orig_values[i] != NULL; i++) {
         sudoku[orig_values[i]->rowID[1] - 1][orig_values[i]->rowID[2] - 1] = orig_values[i]->rowID[0];
     }
 }
 
-//---------------------------------PRINTS A SUDOKU GRID OF ANY SIZE---------------------------------------------//
+// PRINTS A SUDOKU GRID OF ANY SIZE -----------------------------------------//
 void printGrid(puzzleType& grid){
     std::string ext_border = "+", int_border = "|";
     int counter = 1;
@@ -354,7 +358,7 @@ static void loadPuzzles(vector<puzzleType> &puzzles) {
     sIn.close();
 }
 
-//--------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
 
 int main(){
     vector<puzzleType>puzzles;
